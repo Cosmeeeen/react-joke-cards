9265852b8d3c302a14ef247ed1457c120f77575c
Cosmeeeen <iliemcosmin@yahoo.ro>
2021-07-04 02:46:51 +0300

    added particlejs background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Particles from 'react-particles-js';
+import { particlesConfig } from './particlesjs-config'
 import JokeCard from './Components/JokeCard';
 
 function App() {
   return (
     <div className="App">
+      <Particles params={particlesConfig}/>
       <JokeCard />
     </div>
   );
